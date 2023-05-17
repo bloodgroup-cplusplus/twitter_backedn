5996455a6beac6baae4d77dff28d68fa1c19036a
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-05-17 07:22:52 +0530

    added skeleton for crud operations

--- src/index.ts
+++ src/index.ts
@@ -12,8 +12,48 @@ app.get('/', (req,res)=>{
     res.send("Hello world");
 });
 
+//app.METHOD(PATH,HANDLER)
+
+
+
+
+//USER CRUD ENDPOINTS 
+
+//creating a user using post request 
+app.post('/user', (req,res)=>{
+    res.status(501).json({error:"Not implemented"});
+})
+
+
+// List users 
+app.get('/user', (req,res)=>{
+    res.status(501).json({error:"Not implemented"})
+});
+
+
+// get one user via user id from request params 
+app.get('/user/:id', (req,res)=>{
+    res.status(501).json({error:"Not implemented for single user"});
+});
+
+
+// update a user 
+app.put('/user/:id', (req,res)=>{
+    const{id} = req.params; 
+    res.status(501).json({error:`Not implmented:${id}`})
+
+})
+
+// delete user 
+app.delete('user/:id', (req,res)=>{
+    const{id} = req.params ; 
+    res.status(501).json({error:`Not implemented :${id}`});
+})
+
+
+
 // listen to request 
 
 app.listen(3000,()=>{
     console.log("server is running");
-})
+});
