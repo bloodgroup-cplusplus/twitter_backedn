b0c4f5b46cb355e7f5e21998e64b5ffae4de664d
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-05-18 08:52:38 +0530

    handling routes for user and shorthanding path

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -4,32 +4,32 @@ const router = Router();
 //USER CRUD ENDPOINTS 
 
 //creating a user using post request 
-router.post('/user', (req,res)=>{
+router.post('/', (req,res)=>{
     res.status(501).json({error:"Not implemented"});
 })
 
 
 // List users 
-router.get('/user', (req,res)=>{
+router.get('/', (req,res)=>{
     res.status(501).json({error:"Not implemented"})
 });
 
 
 // get one user via user id from request params 
-router.get('/user/:id', (req,res)=>{
+router.get('/:id', (req,res)=>{
     res.status(501).json({error:"Not implemented for single user"});
 });
 
 
 // update a user 
-router.put('/user/:id', (req,res)=>{
+router.put('/:id', (req,res)=>{
     const{id} = req.params; 
     res.status(501).json({error:`Not implmented:${id}`})
 
 })
 
 // delete user 
-router.delete('user/:id', (req,res)=>{
+router.delete('/:id', (req,res)=>{
     const{id} = req.params ; 
     res.status(501).json({error:`Not implemented :${id}`});
 })
